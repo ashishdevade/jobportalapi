06956d9ce1a4a475ff8bdf9c6b6021dece39cf1e
adevade007 <ashish.devade123@gmail.com>
2022-02-23 18:25:41 +0530

    email id updated

--- config/constants.js
+++ config/constants.js
@@ -4,7 +4,7 @@ module.exports =  {
 	"default_image_path":"media/no-image.png",
 	"smtp_host":"smtp.gmail.com",
 	"smtp_port":587,
-	"smtp_user":"ashish.devade456@gmail.com",
-	"smtp_pass":"Ashish@123",
+	"smtp_user":"nisarg205@gmail.com",
+	"smtp_pass":"ZestBoard$9",
 	"smtp_username":' "Zestboard App" contact@zestboard.com',
 }
